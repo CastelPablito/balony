26e7bf76d2c1014d1567efe32de6e90eead6f56f
CastelPablito <vpeeaar@gmail.com>
2019-10-12 22:56:22 +0200

    dd

--- Game/Game/Form1.Designer.cs
+++ Game/Game/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Game
 {
-    partial class Form1
+    partial class Menu
     {
         /// <summary>
         /// Wymagana zmienna projektanta.
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Start = new System.Windows.Forms.Button();
+            this.Wyniki = new System.Windows.Forms.Button();
+            this.Wyjscie = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // Start
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Start.Location = new System.Drawing.Point(71, 80);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(223, 66);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            // 
+            // Wyniki
+            // 
+            this.Wyniki.Location = new System.Drawing.Point(71, 197);
+            this.Wyniki.Name = "Wyniki";
+            this.Wyniki.Size = new System.Drawing.Size(223, 66);
+            this.Wyniki.TabIndex = 1;
+            this.Wyniki.Text = "Wyniki";
+            this.Wyniki.UseVisualStyleBackColor = true;
+            // 
+            // Wyjscie
+            // 
+            this.Wyjscie.Location = new System.Drawing.Point(71, 315);
+            this.Wyjscie.Name = "Wyjscie";
+            this.Wyjscie.Size = new System.Drawing.Size(223, 66);
+            this.Wyjscie.TabIndex = 2;
+            this.Wyjscie.Text = "Wyjście";
+            this.Wyjscie.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(365, 501);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.Wyjscie);
+            this.Controls.Add(this.Wyniki);
+            this.Controls.Add(this.Start);
+            this.Name = "Menu";
+            this.Text = "Boom Balloon!";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Wyniki;
+        private System.Windows.Forms.Button Wyjscie;
     }
 }
 
