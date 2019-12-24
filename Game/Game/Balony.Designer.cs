a5d857f538a3000fc0a547c42abfd24bc429c9e1
CastelPablito <vpeeaar@gmail.com>
2019-12-24 02:28:01 +0100

    added gas info

--- Game/Game/Balony.Designer.cs
+++ Game/Game/Balony.Designer.cs
@@ -60,9 +60,10 @@
             // 
             // Wyjscie
             // 
-            this.Wyjscie.Location = new System.Drawing.Point(767, 639);
+            this.Wyjscie.Location = new System.Drawing.Point(1023, 786);
+            this.Wyjscie.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Wyjscie.Name = "Wyjscie";
-            this.Wyjscie.Size = new System.Drawing.Size(152, 46);
+            this.Wyjscie.Size = new System.Drawing.Size(203, 57);
             this.Wyjscie.TabIndex = 0;
             this.Wyjscie.Text = "Wyjscie";
             this.Wyjscie.UseVisualStyleBackColor = true;
@@ -70,9 +71,10 @@
             // 
             // Pauza
             // 
-            this.Pauza.Location = new System.Drawing.Point(767, 577);
+            this.Pauza.Location = new System.Drawing.Point(1023, 710);
+            this.Pauza.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Pauza.Name = "Pauza";
-            this.Pauza.Size = new System.Drawing.Size(152, 46);
+            this.Pauza.Size = new System.Drawing.Size(203, 57);
             this.Pauza.TabIndex = 2;
             this.Pauza.Text = "Pauza";
             this.Pauza.UseVisualStyleBackColor = true;
@@ -87,17 +89,19 @@
             // 
             this.punkty.AutoSize = true;
             this.punkty.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
-            this.punkty.Location = new System.Drawing.Point(759, 21);
+            this.punkty.Location = new System.Drawing.Point(1012, 26);
+            this.punkty.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.punkty.Name = "punkty";
-            this.punkty.Size = new System.Drawing.Size(181, 46);
+            this.punkty.Size = new System.Drawing.Size(227, 58);
             this.punkty.TabIndex = 3;
             this.punkty.Text = "PUNKTY";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(773, 81);
+            this.progressBar1.Location = new System.Drawing.Point(1031, 100);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(146, 24);
+            this.progressBar1.Size = new System.Drawing.Size(195, 30);
             this.progressBar1.TabIndex = 4;
             // 
             // GameSpace
@@ -115,8 +119,9 @@
             this.GameSpace.Controls.Add(this.wodor);
             this.GameSpace.Controls.Add(this.azot);
             this.GameSpace.Location = new System.Drawing.Point(0, 0);
+            this.GameSpace.Margin = new System.Windows.Forms.Padding(4);
             this.GameSpace.Name = "GameSpace";
-            this.GameSpace.Size = new System.Drawing.Size(758, 712);
+            this.GameSpace.Size = new System.Drawing.Size(1011, 876);
             this.GameSpace.TabIndex = 1;
             this.GameSpace.Paint += new System.Windows.Forms.PaintEventHandler(this.GameSpace_Paint);
             // 
@@ -124,134 +129,154 @@
             // 
             this.chlor.BackColor = System.Drawing.Color.Transparent;
             this.chlor.Image = global::Game.Properties.Resources.chlor;
-            this.chlor.Location = new System.Drawing.Point(112, 584);
+            this.chlor.Location = new System.Drawing.Point(149, 719);
+            this.chlor.Margin = new System.Windows.Forms.Padding(4);
             this.chlor.Name = "chlor";
             this.chlor.Size = new System.Drawing.Size(104, 104);
             this.chlor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.chlor.TabIndex = 9;
             this.chlor.TabStop = false;
             this.chlor.Tag = "chlor";
+            this.chlor.Click += new System.EventHandler(this.chlor_Click);
             // 
             // cyjanowodor
             // 
             this.cyjanowodor.BackColor = System.Drawing.Color.Transparent;
             this.cyjanowodor.Image = global::Game.Properties.Resources.cyjanowodor;
-            this.cyjanowodor.Location = new System.Drawing.Point(222, 577);
+            this.cyjanowodor.Location = new System.Drawing.Point(296, 710);
+            this.cyjanowodor.Margin = new System.Windows.Forms.Padding(4);
             this.cyjanowodor.Name = "cyjanowodor";
             this.cyjanowodor.Size = new System.Drawing.Size(106, 111);
             this.cyjanowodor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.cyjanowodor.TabIndex = 8;
             this.cyjanowodor.TabStop = false;
             this.cyjanowodor.Tag = "cyjanowodor";
+            this.cyjanowodor.Click += new System.EventHandler(this.cyjanowodor_Click);
             // 
             // co2
             // 
             this.co2.BackColor = System.Drawing.Color.Transparent;
             this.co2.Image = global::Game.Properties.Resources.dwutlenek_wegla;
-            this.co2.Location = new System.Drawing.Point(334, 577);
+            this.co2.Location = new System.Drawing.Point(445, 710);
+            this.co2.Margin = new System.Windows.Forms.Padding(4);
             this.co2.Name = "co2";
             this.co2.Size = new System.Drawing.Size(107, 101);
             this.co2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.co2.TabIndex = 7;
             this.co2.TabStop = false;
             this.co2.Tag = "co2";
+            this.co2.Click += new System.EventHandler(this.co2_Click);
             // 
             // hel
             // 
             this.hel.BackColor = System.Drawing.Color.Transparent;
             this.hel.Image = global::Game.Properties.Resources.hel;
-            this.hel.Location = new System.Drawing.Point(447, 577);
+            this.hel.Location = new System.Drawing.Point(596, 710);
+            this.hel.Margin = new System.Windows.Forms.Padding(4);
             this.hel.Name = "hel";
             this.hel.Size = new System.Drawing.Size(90, 96);
             this.hel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.hel.TabIndex = 6;
             this.hel.TabStop = false;
             this.hel.Tag = "hel";
+            this.hel.Click += new System.EventHandler(this.hel_Click);
             // 
             // metan
             // 
             this.metan.BackColor = System.Drawing.Color.Transparent;
             this.metan.Image = global::Game.Properties.Resources.metan;
-            this.metan.Location = new System.Drawing.Point(543, 562);
+            this.metan.Location = new System.Drawing.Point(724, 692);
+            this.metan.Margin = new System.Windows.Forms.Padding(4);
             this.metan.Name = "metan";
             this.metan.Size = new System.Drawing.Size(123, 102);
             this.metan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.metan.TabIndex = 5;
             this.metan.TabStop = false;
             this.metan.Tag = "metan";
+            this.metan.Click += new System.EventHandler(this.metan_Click);
             // 
             // siarkowodor
             // 
             this.siarkowodor.BackColor = System.Drawing.Color.Transparent;
             this.siarkowodor.Image = global::Game.Properties.Resources.siarkowodor;
-            this.siarkowodor.Location = new System.Drawing.Point(595, 400);
+            this.siarkowodor.Location = new System.Drawing.Point(793, 492);
+            this.siarkowodor.Margin = new System.Windows.Forms.Padding(4);
             this.siarkowodor.Name = "siarkowodor";
             this.siarkowodor.Size = new System.Drawing.Size(107, 101);
             this.siarkowodor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.siarkowodor.TabIndex = 4;
             this.siarkowodor.TabStop = false;
             this.siarkowodor.Tag = "siarkowodor";
+            this.siarkowodor.Click += new System.EventHandler(this.siarkowodor_Click);
             // 
             // tlen
             // 
             this.tlen.BackColor = System.Drawing.Color.Transparent;
             this.tlen.Image = global::Game.Properties.Resources.tlen;
-            this.tlen.Location = new System.Drawing.Point(472, 418);
+            this.tlen.Location = new System.Drawing.Point(629, 514);
+            this.tlen.Margin = new System.Windows.Forms.Padding(4);
             this.tlen.Name = "tlen";
             this.tlen.Size = new System.Drawing.Size(90, 96);
             this.tlen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.tlen.TabIndex = 3;
             this.tlen.TabStop = false;
             this.tlen.Tag = "tlen";
+            this.tlen.Click += new System.EventHandler(this.tlen_Click);
             // 
             // czad
             // 
             this.czad.BackColor = System.Drawing.Color.Transparent;
             this.czad.Image = global::Game.Properties.Resources.tlenek_wegla;
-            this.czad.Location = new System.Drawing.Point(360, 418);
+            this.czad.Location = new System.Drawing.Point(480, 514);
+            this.czad.Margin = new System.Windows.Forms.Padding(4);
             this.czad.Name = "czad";
             this.czad.Size = new System.Drawing.Size(123, 102);
             this.czad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.czad.TabIndex = 2;
             this.czad.TabStop = false;
             this.czad.Tag = "czad";
+            this.czad.Click += new System.EventHandler(this.czad_Click);
             // 
             // wodor
             // 
             this.wodor.BackColor = System.Drawing.Color.Transparent;
             this.wodor.Image = global::Game.Properties.Resources.wodór;
-            this.wodor.Location = new System.Drawing.Point(222, 418);
+            this.wodor.Location = new System.Drawing.Point(296, 514);
+            this.wodor.Margin = new System.Windows.Forms.Padding(4);
             this.wodor.Name = "wodor";
             this.wodor.Size = new System.Drawing.Size(104, 104);
             this.wodor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.wodor.TabIndex = 1;
             this.wodor.TabStop = false;
             this.wodor.Tag = "wodor";
+            this.wodor.Click += new System.EventHandler(this.wodor_Click);
             // 
             // azot
             // 
             this.azot.BackColor = System.Drawing.Color.Transparent;
             this.azot.Image = global::Game.Properties.Resources.azot;
-            this.azot.Location = new System.Drawing.Point(0, 584);
+            this.azot.Location = new System.Drawing.Point(0, 719);
+            this.azot.Margin = new System.Windows.Forms.Padding(4);
             this.azot.Name = "azot";
             this.azot.Size = new System.Drawing.Size(106, 111);
             this.azot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.azot.TabIndex = 0;
             this.azot.TabStop = false;
             this.azot.Tag = "azot";
-            this.azot.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.azot.Click += new System.EventHandler(this.azot_Click);
             // 
             // Balony
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(936, 710);
+            this.ClientSize = new System.Drawing.Size(1248, 874);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.punkty);
             this.Controls.Add(this.Pauza);
             this.Controls.Add(this.GameSpace);
             this.Controls.Add(this.Wyjscie);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Balony";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Boom Balloon!";
--- Game/Game/Balony.cs
+++ Game/Game/Balony.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Drawing;
 using System.Threading.Tasks;
 
+
 namespace Game
 {
     public partial class Balony : Form
@@ -17,7 +18,9 @@ namespace Game
         int count = 0;
         //int speed = 5;
         int score = 0;
-
+        int il_O2 = 21;
+        int il_N2 = 78;
+        int il_C02 = 1; 
         Balon balonik = new Balon(600);
 
         public Balony()
@@ -25,7 +28,7 @@ namespace Game
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.ShowDialog();
-            
+
         }
 
         private void Wyjscie_Click(object sender, EventArgs e)
@@ -48,18 +51,118 @@ namespace Game
             GameSpace.Refresh();
             // GameSpace.Invalidate();
             count++;
-            punkty.Text = ""+score;
+            punkty.Text = "" + score;
             if (count >= 1)
             {
                 balonik.y--;
-              //  score += 10;
+                //  score += 10;
             }
-            
+
 
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+
+        // KOMUNIKATY
+
+        string czad_info = "⚝ CO - tlenek węgla, potocznie nazywany czadem.\n" +
+                           "⚝ Jest bezbarwnym i bezwonnym gazem powstającym w skutek spalania węgla w niedostatecznej ilości tlenu.\n" +
+                           "⚝ W czasie zimy zdarzają się zatrucia tym gazem spowodowane nieprawidłowym korzystaniem z instalacji ciepłowniczych\n" +
+                           "⚝ Zbyt duże stężenie tego gazu w powietrzu może powodować poważne zatrucie a nawet śmierć.\n";
+
+        string wodor_info = "⚝ Wodór jest najczęściej występującym pierwiastkiem we wszechświecie.\n" +
+                            "Stanowi on zdecydowaną większość masy słońca, mimo że jest najlżejszym z pierwiastków\n" +
+                            "⚝ W naturze naczęściej występuje w postaci dwuatomowej\n" +
+                            "⚝ W związku z jego lekkością był wykorzystywany między innymi w statkach powietrznych zwanych sterowcami\n" +
+                            "⚝ Niestety, w reakcji z tlenem jest bardzo wybuchowy, co doprowadziło do katastrofy, która przekreśliła przyszłość sterowców\n" +
+                            "⚝ Dzisiaj wykorzystywany jest jako wysokoenergetyczne paliwo\n";
+
+        string siarkowodor_info = "⚝ Siarkowodór zapachem przypomina zapach zgniłych jaj.\n" +
+                                  "⚝ Choć może wydać się to dziwne, ale to jego największa zaleta, gdyż gaz ten jest bardzo niebezpieczny\n" +
+                                  "⚝ Siarkowodór działa toksycznie na układ oddechowy oraz nerwowy, zbyt duża jego ilość powoduje zatrzymanie oddechu, a w rezultacie śmierć\n" +
+                                  "⚝ Gaz ten występuje naturalnie wydobywając się ze szczelin w okolicach wulkanu, jak i w beztlenowym rozkladzie białek\n" +
+                                  "⚝ Co ciekawe ludzki organizm również potrafi produkować ten gaz, co da się wyczuć kiedy stary najebany wychodzi z toalety\n";
+
+        string chlor_info = "⚝ Chlor jest silnie trującym żółtozielonym gazem o nieprzyjemnym zapachu.\n" +
+                            "⚝ Jego obecność w powietrzu może prowadzić do obrzęku płuc, a nawet do śmierci\n" +
+                            "⚝ Podczas I Wojny Światowej był stosowany jako broń chemiczna\n" +
+                            "⚝ Mimo jego okropnych właściwości jako gazu, chlor jest bardzo przydatnym pierwiastkiem\n" +
+                            "⚝ Używa się go w celu uzdatniania wody\n"+
+                            "⚝ Jest składnikiem soli kuchennej (NaCl)\n"+
+                            "⚝ Oraz pomaga nam w trawieniu, gdyż kwas chlorowodorowy (HCl) znajduje się w naszym żołądku!\n";
+
+        string cyjanowodor_info = "⚝ Cyjanowodór to silnie toksyczny gaz wchłaniający się przez płuca oraz skórę.\n" +
+                                  "⚝ Zapachem przypomina gorzkie migdały.\n "+
+                                  "⚝ Bardzo niewielka ilość może spowodować zgon.\n " +
+                                  "⚝ W czasie II Wojny Światowej był stosowany do trucia więźniów w niemieckich obozach zagłady\n ";
+                                 
+        string metan_info = "⚝ Metan jest bezbawrnym i bezwonnym silnie wybuchowym gazem\n"+
+                             "⚝ Jego obecność często świadczy o obecności prostego życia, gdyż jest on wydalany przez proste bakterie\n"+
+                             "Górnicy schodząc do kopalni muszą uważać na stężenie tego gazu\n"+
+                             "⚝ Jego obecność tam może prowadzić do wybuchu oraz zawalenia się kopalni\n"+
+                             "⚝ Jest jednym ze składników gazu ziemnego\n"+
+                             "⚝ Jest on gazem cieplarnianym, a więc zbyt duża jego obecność w atmosferze może przyczyniać się do niszczenia wartswy ozonowej\n";
+
+        // AKCJA
+
+        private void tlen_Click(object sender, EventArgs e)
         {
+            score += 10;
+            il_O2++;
+        }
+
+        private void azot_Click(object sender, EventArgs e)
+        {
+            score += 10;
+            il_N2++;
+        }
+
+        private void co2_Click(object sender, EventArgs e)
+        {
+            score += 10;
+            il_C02++;
+        }
+
+        private void hel_Click(object sender, EventArgs e)
+        {
+            score += 10;
+        }
+   
+        private void czad_Click(object sender, EventArgs e)
+        {
+            info(czad_info);
+        }
+
+        private void wodor_Click(object sender, EventArgs e)
+        {
+            info(wodor_info);
+        }
+
+        private void siarkowodor_Click(object sender, EventArgs e)
+        {
+            info(siarkowodor_info);
+        }
+
+        private void chlor_Click(object sender, EventArgs e)
+        {
+            info(chlor_info);
+        }
+
+        private void cyjanowodor_Click(object sender, EventArgs e)
+        {
+            info(cyjanowodor_info);
+        }
+
+        private void metan_Click(object sender, EventArgs e)
+        {
+            info(metan_info);
+        }
+
+        void info(string x)
+        {
+            //MessageBox info = new MessageBox();
+            // MessageBox dupa = new MessageBox();
+            timer.Stop();
+            MessageBox.Show(x, "Koniec gry!");
 
         }
     }
